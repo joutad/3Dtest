180e9f41ef88d79e6f916dedfe9a275b20510fa8
Joudat <joudat01@gmail.com>
2024-02-25 16:00:54 -0500

    Added camera control with mouse, fixed positioning

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
 import {Canvas} from "@react-three/fiber";
 import {Suspense} from "react";
+import {OrbitControls} from "@react-three/drei";
 import Island from './Models/Island.jsx'
+import './App.css'
 
 function App() {
-  return (
+    return (
     <div className="App">
-        <Canvas>
-            <Suspense fallback={null}>
-                <Island/>
-            </Suspense>
-        </Canvas>
+        <div className='canvas-container'>
+            <h1>Your model</h1>
+            <Canvas className='canvas' camera={{position: [0, 90, -325]}}>
+                <Suspense fallback={null}>
+                    <Island position={[0, 0, 0]}/>
+                    <OrbitControls/>
+                </Suspense>
+            </Canvas>
+        </div>
     </div>
-  )
+    )
 }
 
 export default App
